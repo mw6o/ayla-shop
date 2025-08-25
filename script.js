0cc1e3bdb1090d97b79c6e91263f93c8a0856d0d
mw6o <king1234a1234@gmail.com>
2025-08-25 10:04:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+﻿function convertToIQD(button) {
+  const exchangeRate = 1320; // سعر صرف الدولار مقابل الدينار العراقي
+  const card = button.closest('.product-card');
+  const usdPrice = parseFloat(card.querySelector('.usd-price').textContent);
+  const iqdPrice = Math.round(usdPrice * exchangeRate);
+  card.querySelector('.iqd-price').textContent = iqdPrice.toLocaleString();
+}
+`
